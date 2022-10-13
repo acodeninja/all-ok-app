2548b715720d6168f034b164745983d8c63b5a0e
Lawrence <lawrence@acode.ninja>
2022-10-13 09:07:16 +0100

    Add some json log messages

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express()
 const port = process.env.PORT || 80
 
 app.all(/^.+$/, (req, res) => {
-  res.status(200).json({status: 'ok'})
+    const {path, hostname, ip} = req;
+    console.log(JSON.stringify({event: 'request', path, hostname, ip}));
+    res.status(200).json({status: 'ok'})
 })
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+    console.log(JSON.stringify({event: 'listening', message: `listening on port ${port}`}))
 })
 
