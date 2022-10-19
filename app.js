fd0c7de57b765969ace3a7afdd9a431e7927d74e
Lawrence <lawrence@acode.ninja>
2022-10-19 15:12:35 +0100

    Change response to something that makes a bit more sense for the context

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 80
 app.all(/^.+$/, (req, res) => {
     const {path, hostname, ip} = req;
     console.log(JSON.stringify({event: 'request', path, hostname, ip}));
-    res.status(200).json({status: 'ok'})
+    res.status(200).send("<h1>Holding Page</h1><p>Services will resume shortly</p>")
 })
 
 app.listen(port, () => {
